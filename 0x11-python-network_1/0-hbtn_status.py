da232a64c52e1507905701e9ca50155e223d22c8
CodeMachne <abangfire7@gmail.com>
2024-07-05 14:06:26 +0100

    added if __name__ == "__main__": to the file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+'''the urllib is used for fetching requests from http headers'''
 import urllib.request
 req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+if __name__ == "__main__":
 with urllib.request.urlopen(req)as status:
     html = status.read()
