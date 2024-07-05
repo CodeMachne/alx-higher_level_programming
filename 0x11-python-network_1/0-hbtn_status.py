bebada9fbcba8fead2cd92252f15c8f9ad82d4de
CodeMachne <abangfire7@gmail.com>
2024-07-05 14:57:24 +0100

    corrected 0-

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 '''the urllib is used for fetching requests from http headers'''
 import urllib.request
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 if __name__ == "__main__":
-with urllib.request.urlopen(req)as status:
-    html = status.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as status:
+        #print("- content: ", +html)
+        html = status.read()
+        #print("- type: ", + type(html))
+        print("\t- content: {}".format(html))
+        print("\t- type: {}".format(type(html)))
+        hhtml = status.read().decode('utf-8')
+        print("\t- utf8 content: {}".format(hhtml))
